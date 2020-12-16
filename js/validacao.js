a5f9221d84d2fc1c7eef5418232b5631fa13cdb9
ikyrie <mah.c5r@gmail.com>
2020-12-16 03:13:45 -0300

    video5.2

--- js/validacao.js
+++ js/validacao.js
@@ -44,12 +44,22 @@ const mensagensDeErro = {
     cep: {
         valueMissing: 'O campo de CEP não pode estar vazio.',
         patternMismatch: 'O CEP digitado não é válido.'
+    },
+    logradouro: {
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo de cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo de estado não pode estar vazio.'
     }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input)
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -147,9 +157,24 @@ function confirmaDigito(soma) {
     return 11 - (soma % 11)
 }
 
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
 
-// 123 456 789 09
-
-// let soma = (11 * 1) + (10 * 2) + (9 * 3) ... (2 * 0)
-
-// let digitoVerificador = 11 - (soma % 11)
+    if(!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                console.log(data)
+            }
+        )
+    }
+}
